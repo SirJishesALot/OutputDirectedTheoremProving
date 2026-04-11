aeb1eb49b29ef72b88574bb7564d177db9b897db
Jishnu Raychaudhuri <70213829+SirJishesALot@users.noreply.github.com>
2026-04-11 19:58:49 +0000

    updated setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,7 +3,7 @@ set -e
 
 # System & Opam Setup (Rocq 9.0)
 sudo apt-get update && sudo apt-get install -y opam libgmp-dev pkg-config nodejs npm
-opam init --disabled-switch -y
+opam init --bare --disable-sandboxing -y
 opam switch create rocq-9.0 ocaml-base-compiler.5.4.1 -y
 eval $(opam env --switch=rocq-9.0)
 opam repo add rocq-released https://rocq-prover.org/opam/released -y
